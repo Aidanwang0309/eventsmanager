e7049edac43d4cfac203e31d47b32fa57320eece
Aidanwang0309 <aidanwang0309@gmail.com>
2019-07-18 12:18:35 -0400

    prod

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const connectDB = require("./config/db");
 connectDB();
@@ -8,13 +9,19 @@ const PORT = process.env.port || 5000;
 
 app.use(express.json({ extended: false }));
 
-// app.get("/", (req, res) =>
-//   res.json({ msg: "Welcome to the contact keeper api" })
-// );
-
 // Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
+
+// Server config in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 app.listen(PORT, () => console.log(`LISTENING TO PORT ${PORT}`));
